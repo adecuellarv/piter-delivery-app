9ecd3ed7b2c2db423771c177866482584a33abe4
adecuellarv <ade.cuellar92@gmail.com>
2025-10-01 08:44:07 -0600

    con layout

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { DeliveryHome } from './src/pages/DeliveryHome';
+import './global.css'; 
 
 export default function App() {
   return (
--- ____.js
+++ ____.js
@@ -1,11 +1,7 @@
-// babel.config.cjs
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      'expo-router/babel',
-      'nativewind/babel',
-    ],
+    plugins: ['react-native-reanimated/plugin'], // SOLO este
   };
 };
